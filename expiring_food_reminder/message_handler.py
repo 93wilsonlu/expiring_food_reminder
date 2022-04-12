64317287a2ad8fd5e875e7552de02cc7c265a76c
93wilsonlu <93wilsonlu@gmail.com>
2022-04-12 16:46:32 +0800

    完成查看功能

--- expiring_food_reminder/message_handler.py
+++ expiring_food_reminder/message_handler.py
@@ -1,4 +1,4 @@
-from expiring_food_reminder import line_bot_api, handler, db
+from expiring_food_reminder import line_bot_api, db
 from linebot.models import TextSendMessage
 from .model import Food
 from datetime import datetime, timedelta
@@ -31,18 +31,43 @@ def handle_add(event):
                 expiry_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) + \
                     timedelta(days=int(time_string) * 365)
             else:
-                raise InputFormatError('The format of time is wrong!')
+                raise InputFormatError('格式錯誤')
         except:
-            raise InputFormatError('The format of time is wrong!')
+            raise InputFormatError('格式錯誤')
 
     food = Food(food_name=food_name, owner_id=event.source.user_id,
                 expiry_time=expiry_time)
     db.session.add(food)
     db.session.commit()
 
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text="加入成功"))
+
 
 def handle_read(event):
-    pass
+    read_method = event.message.text.split(' ')[1]
+    foods = Food.query.filter_by(owner_id=event.source.user_id)
+    if read_method == 'count':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(
+            text=str(foods.count())))
+        return
+
+    if read_method == 'all':
+        food_list = foods.all()
+    elif read_method == 'expiring':
+        food_list = foods.filter(
+            Food.expiry_time == datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)).all()
+    elif read_method == 'expired':
+        food_list = foods.filter(
+            Food.expiry_time < datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)).all()
+    else:
+        raise InputFormatError('格式錯誤')
+
+    result = '以下是查詢結果:'
+    for food in food_list:
+        result += f"\n{food.id} {food.food_name} {food.expiry_time.strftime('%Y-%m-%d')}"
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=result))
 
 
 def handle_edit(event):
